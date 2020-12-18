c238ad74d9aeeeb7db775491cd0f558c52dac008
dhess <dan.hess@kippnorcal.org>
2020-12-18 11:36:59 -0800

    Add docstrings and include future sprints.

--- main.py
+++ main.py
@@ -35,9 +35,15 @@ class Connector:
         self.jira = Jira(url=url, username=username, password=password)
 
     def table_name(self, name):
+        """
+        Simple function to add a consistent prefix to table names.
+        """
         return f"jira_{name}"
 
     def get_projects(self):
+        """
+        Extract project data from Jira and load into database table.
+        """
         table = self.table_name("projects")
         columns = ["id", "key", "name", "projectTypeKey", "style", "isPrivate"]
         projects = self.jira.get_all_projects()
@@ -47,6 +53,10 @@ class Connector:
         logging.info(f"Loaded {len(projects)} projects into {table}")
 
     def get_boards(self):
+        """
+        Extract board data from Jira and load into database table. This
+        is needed for querying sprints.
+        """
         table = self.table_name("boards")
         columns = ["id", "name", "type", "location_projectId"]
         boards = self.jira.get_all_agile_boards()
@@ -56,6 +66,9 @@ class Connector:
         logging.info(f"Loaded {len(boards)} boards into {table}")
 
     def get_sprints(self):
+        """
+        Extract sprint data from Jira and load into database table.
+        """
         table = self.table_name("sprints")
         sprints = self.jira.get_all_sprint("10")  # TODO: Get this board_id dynamically
         dates = ["startDate", "endDate", "completeDate"]
@@ -66,21 +79,21 @@ class Connector:
         logging.info(f"Loaded {len(sprints)} sprints into {table}")
 
     def get_sprint_ids(self, active=False):
+        """
+        Return a list of sprint IDs to use for querying issues. Optional
+        param can be passed to only return active and future sprints. 
+        """
         table = self.table_name("sprints")
         df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
         if active:
-            df = df[df["state"] == "active"]
-        else:
-            df = df[df["state"] != "future"]
+            df = df[df["state"] != "closed"]
         return df[["id"]].values.flatten().tolist()
 
-    def get_active_sprint(self):
-        table = self.table_name("sprints")
-        df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
-        df = df[df["state"] == "active"]
-        return df[["id"]].values.flatten().tolist()[0]
-
     def get_sprint_issues(self, sprint_id):
+        """
+        Extract issue data for a given sprint from Jira and load into
+        database table.
+        """
         table = self.table_name("issues")
         columns = {
             "id": "id",
@@ -120,28 +133,45 @@ class Connector:
         logging.info(f"Loaded {len(issues)} issues for sprint {sprint_id} into {table}")
 
     def get_all_issues(self):
+        """
+        Loads issues for multiple sprints. On first loading it will pull
+        all sprints. On subsequent runs, it will drop any issues for
+        active and future sprints and re-query them.
+        """
         table_name = self.table_name("issues")
         if self.table_exists(table_name):
             sprints = self.get_sprint_ids(active=True)
-            self.delete_sprint_issues(sprints[0])
+            self.delete_sprint_issues(sprints)
         else:
             sprints = self.get_sprint_ids()
         for sprint in sprints:
             self.get_sprint_issues(sprint)
 
     def table_exists(self, table_name):
+        """
+        Checks if a table already exists in the database. Used for 
+        determining what actions to take for loading depending on
+        the existence of prior loaded data.
+        """
         return self.sql.engine.dialect.has_table(
             connection=self.sql.engine, tablename=table_name, schema=self.sql.schema
         )
 
-    def delete_sprint_issues(self, sprint):
+    def delete_sprint_issues(self, sprints):
+        """
+        Deletes records in the issues table for a given set of sprints.
+        """
         table_name = self.table_name("issues")
         if self.table_exists(table_name):
             table = self.sql.table(table_name)
-            d = table.delete().where(table.c.sprint == sprint)
+            d = table.delete().where(table.c.sprint.in_(sprints))
             self.sql.engine.execute(d)
 
     def get_issue_changes(self, issue_key, count, total):
+        """
+        Extract change history for a given issue by key. Expects params
+        for a count and expected total to be passed in for debug logging.
+        """
         table = self.table_name("issue_changes")
 
         changes = self.jira.get_issue_changelog(issue_key)
@@ -169,29 +199,50 @@ class Connector:
             df = df[columns]
             # TODO: check if id in issue_changes, drop from df if already exists (only load new ones)
             self.sql.insert_into(table, df, dtype={"created": DateTime})
-            logging.info(
+            logging.debug(
                 f"Loaded {len(df)} changes for {issue_key} into {table} {count}/{total}"
             )
 
     def get_issue_keys(self, active=False):
+        """
+        Return a distinct list (set) of issue keys for use in querying
+        the change history. If the active param is passed it will only
+        pull issues in active or future sprints.
+        """
         table = self.table_name("issues")
         df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
         if active:
-            active_sprint = self.get_sprint_ids(active=True)[0]
-            df = df[df["sprint"] == active_sprint]
+            active_sprints = self.get_sprint_ids(active=True)
+            df = df[df["sprint"] in active_sprints]
         return set(df[["key"]].values.flatten().tolist())
 
     def get_issue_change_keys(self):
+        """
+        Queries a distinct list (set) of issue keys from the change
+        history. Used to determine which changes to query for.
+        """
         table = self.table_name("issue_changes")
         df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
         return set(df[["issue_key"]].values.flatten().tolist())
 
     def get_issue_key_diff(self):
+        """
+        Returns the distinct set of issue keys that exist in the issue
+        table but not in the change table.
+        """
+        table_name = self.table_name("issue_changes")
         issue_keys = self.get_issue_keys()
-        issue_change_keys = self.get_issue_change_keys()
-        return issue_keys - issue_change_keys
+        if self.table_exists(table_name):
+            issue_change_keys = self.get_issue_change_keys()
+            return issue_keys - issue_change_keys
+        else:
+            return issue_keys
 
     def delete_issue_changes(self):
+        """
+        Delete records in the issue changes table that match a set of
+        issue keys.
+        """
         table_name = self.table_name("issue_changes")
         if self.table_exists(table_name):
             table = self.sql.table(table_name)
@@ -200,6 +251,9 @@ class Connector:
             self.sql.engine.execute(d)
 
     def get_all_changes(self):
+        """
+        Extract issue changes and load in database table.
+        """
         # drop matching keys from tables
         self.delete_issue_changes()
         # query issue keys in issues but not in issue_changes

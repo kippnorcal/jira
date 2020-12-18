fbda9ee0eaa15a894ad50ec7dc3e67d1dded5759
dhess <dan.hess@kippnorcal.org>
2020-12-17 16:12:37 -0800

    Add connector class and methods for extracting scrum data

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import traceback
 from atlassian import Jira
 import pandas as pd
 from sqlsorcery import MSSQL
+from sqlalchemy.types import DateTime
 
 from mailer import Mailer
 from timer import elapsed
@@ -25,85 +26,203 @@ def configure_logging():
     )
 
 
-def get_nested(issue, field, subfield):
-    if not issue:
-        return
-    if issue.get(field):
-        return issue.get(field).get(subfield)
+class Connector:
+    def __init__(self):
+        self.sql = MSSQL()
+        url = f'https://{os.getenv("JIRA_URL")}.atlassian.net'
+        username = os.getenv("JIRA_USER")
+        password = os.getenv("JIRA_TOKEN")
+        self.jira = Jira(url=url, username=username, password=password)
 
+    def table_name(self, name):
+        return f"jira_{name}"
 
-def format_record(issue):
-    fields = issue.get("fields")
-    return {
-        "id": issue.get("id"),
-        "key": issue.get("key"),
-        "issue_type": get_nested(fields, "issuetype", "name"),
-        "parent": get_nested(fields, "parent", "key"),
-        "status": get_nested(fields, "status", "name"),
-        "priority": get_nested(fields, "priority", "name"),
-        "estimate": fields.get("customfield_10015"),
-        "summary": fields.get("summary"),
-        "strategic_goal": get_nested(fields, "customfield_10040", "value"),
-        "assignee": get_nested(fields, "assignee", "displayName"),
-        "start_date": fields.get("customfield_10014"),
-        "due_date": fields.get("duedate"),
-        "created": fields.get("created"),
-        "updated": fields.get("updated"),
-    }
+    def get_projects(self):
+        table = self.table_name("projects")
+        columns = ["id", "key", "name", "projectTypeKey", "style", "isPrivate"]
+        projects = self.jira.get_all_projects()
+        df = pd.json_normalize(projects, sep="_", errors="ignore")
+        df = df[columns]
+        self.sql.insert_into(table, df, if_exists="replace")
+        logging.info(f"Loaded {len(projects)} projects into {table}")
 
+    def get_boards(self):
+        table = self.table_name("boards")
+        columns = ["id", "name", "type", "location_projectId"]
+        boards = self.jira.get_all_agile_boards()
+        df = pd.json_normalize(boards["values"], sep="_", errors="ignore")
+        df = df[columns]
+        self.sql.insert_into(table, df, if_exists="replace")
+        logging.info(f"Loaded {len(boards)} boards into {table}")
 
-def get_all_issues(jira, project, fields):
-    issues = []
-    start = 0
-    while True:
-        data = jira.jql(
-            f"project={project} & updated >= -1d",
-            start=start,
-            limit=100,
-            fields=fields,
+    def get_sprints(self):
+        table = self.table_name("sprints")
+        sprints = self.jira.get_all_sprint("10")  # TODO: Get this board_id dynamically
+        dates = ["startDate", "endDate", "completeDate"]
+        df = pd.json_normalize(sprints["values"], sep="_", errors="ignore")
+        df.drop(["self"], axis=1, inplace=True)
+        df = df.astype({col: "datetime64[ns]" for col in dates})
+        self.sql.insert_into(table, df, if_exists="replace")
+        logging.info(f"Loaded {len(sprints)} sprints into {table}")
+
+    def get_sprint_ids(self, active=False):
+        table = self.table_name("sprints")
+        df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
+        if active:
+            df = df[df["state"] == "active"]
+        else:
+            df = df[df["state"] != "future"]
+        return df[["id"]].values.flatten().tolist()
+
+    def get_active_sprint(self):
+        table = self.table_name("sprints")
+        df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
+        df = df[df["state"] == "active"]
+        return df[["id"]].values.flatten().tolist()[0]
+
+    def get_sprint_issues(self, sprint_id):
+        table = self.table_name("issues")
+        columns = {
+            "id": "id",
+            "key": "key",
+            "fields_issuetype_name": "issue_type",
+            "fields_project_id": "project",
+            "fields_epic": "epic",
+            "fields_status_name": "status",
+            "fields_priority_name": "priority",
+            "fields_customfield_10015": "estimate",
+            "fields_summary": "summary",
+            "fields_assignee_displayName": "assignee",
+            "fields_creator_displayName": "creator",
+            "fields_duedate": "due_date",
+            "fields_created": "created",
+            "fields_updated": "updated",
+        }
+        issues = []
+        start = 0
+        while True:
+            data = self.jira.get_sprint_issues(
+                sprint_id=sprint_id, start=start, limit=100
+            )
+            issues.extend(data["issues"])
+            start = len(issues)
+            if start >= data["total"]:
+                break
+        df = pd.json_normalize(issues, sep="_", errors="ignore")
+        df = df[columns.keys()]
+        df.rename(columns=columns, inplace=True)
+        df["created"] = pd.to_datetime(df["created"], utc=True)
+        df["updated"] = pd.to_datetime(df["updated"], utc=True)
+        df["sprint"] = sprint_id
+        self.sql.insert_into(
+            table, df, dtype={"created": DateTime, "updated": DateTime}
         )
-        records = [format_record(issue) for issue in data["issues"]]
-        issues.extend(records)
-        start = len(issues)
-        if start >= data["total"]:
-            break
+        logging.info(f"Loaded {len(issues)} issues for sprint {sprint_id} into {table}")
 
-    return issues
+    def get_all_issues(self):
+        table_name = self.table_name("issues")
+        if self.table_exists(table_name):
+            sprints = self.get_sprint_ids(active=True)
+            self.delete_sprint_issues(sprints[0])
+        else:
+            sprints = self.get_sprint_ids()
+        for sprint in sprints:
+            self.get_sprint_issues(sprint)
+
+    def table_exists(self, table_name):
+        return self.sql.engine.dialect.has_table(
+            connection=self.sql.engine, tablename=table_name, schema=self.sql.schema
+        )
+
+    def delete_sprint_issues(self, sprint):
+        table_name = self.table_name("issues")
+        if self.table_exists(table_name):
+            table = self.sql.table(table_name)
+            d = table.delete().where(table.c.sprint == sprint)
+            self.sql.engine.execute(d)
+
+    def get_issue_changes(self, issue_key, count, total):
+        table = self.table_name("issue_changes")
+
+        changes = self.jira.get_issue_changelog(issue_key)
+        if changes:
+            df = pd.json_normalize(
+                changes["histories"],
+                sep="_",
+                record_path=["items"],
+                meta=["id", "created", "author"],
+                errors="ignore",
+            )
+            df["issue_key"] = issue_key
+            df["author"] = df["author"].map(lambda a: a.get("displayName"))
+            df["created"] = pd.to_datetime(df["created"], utc=True)
+            columns = [
+                "issue_key",
+                "id",
+                "created",
+                "author",
+                "field",
+                "fieldtype",
+                "fromString",
+                "toString",
+            ]
+            df = df[columns]
+            # TODO: check if id in issue_changes, drop from df if already exists (only load new ones)
+            self.sql.insert_into(table, df, dtype={"created": DateTime})
+            logging.info(
+                f"Loaded {len(df)} changes for {issue_key} into {table} {count}/{total}"
+            )
+
+    def get_issue_keys(self, active=False):
+        table = self.table_name("issues")
+        df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
+        if active:
+            active_sprint = self.get_sprint_ids(active=True)[0]
+            df = df[df["sprint"] == active_sprint]
+        return set(df[["key"]].values.flatten().tolist())
+
+    def get_issue_change_keys(self):
+        table = self.table_name("issue_changes")
+        df = pd.read_sql_table(table, con=self.sql.engine, schema=self.sql.schema)
+        return set(df[["issue_key"]].values.flatten().tolist())
+
+    def get_issue_key_diff(self):
+        issue_keys = self.get_issue_keys()
+        issue_change_keys = self.get_issue_change_keys()
+        return issue_keys - issue_change_keys
+
+    def delete_issue_changes(self):
+        table_name = self.table_name("issue_changes")
+        if self.table_exists(table_name):
+            table = self.sql.table(table_name)
+            keys = self.get_issue_keys(active=True)
+            d = table.delete().where(table.c.issue_key.in_(keys))
+            self.sql.engine.execute(d)
+
+    def get_all_changes(self):
+        # drop matching keys from tables
+        self.delete_issue_changes()
+        # query issue keys in issues but not in issue_changes
+        keys = self.get_issue_key_diff()
+        total = len(keys)
+
+        for index, key in enumerate(keys):
+            try:
+                self.get_issue_changes(key, index + 1, total)
+            except Exception as e:
+                print(e)
+                print(key)
 
 
 @elapsed
 def main():
     configure_logging()
-    sql = MSSQL()
-    url = f'https://{os.getenv("JIRA_URL")}.atlassian.net'
-    username = os.getenv("JIRA_USER")
-    password = os.getenv("JIRA_TOKEN")
-    jira = Jira(url=url, username=username, password=password,)
-    fields = [
-        "issuetype",
-        "parent",
-        "summary",
-        "assignee",
-        "created",
-        "priority",
-        "updated",
-        "status",
-        "customfield_10040",  # strategic_goal
-        "customfield_10014",  # start date
-        "customfield_10015",  # story points
-        "duedate",
-    ]
-    dates = ["start_date", "due_date", "created", "updated"]
-    issues = get_all_issues(jira, "PROJ21", fields)
-    if issues:
-        new = pd.DataFrame(issues)
-        new = new.astype({col: "datetime64[ns]" for col in dates})
-        table_name = "jira_Issues"
-        old = pd.read_sql_table(table_name, con=sql.engine, schema=sql.schema)
-        df = pd.concat([old, new])
-        df.drop_duplicates(keep="last", inplace=True, subset=["id"])
-        sql.insert_into(table_name, df, if_exists="replace")
-        logging.info(f"Inserted {len(df)} rows into {table_name}")
+    connector = Connector()
+    connector.get_projects()
+    connector.get_boards()
+    connector.get_sprints()
+    connector.get_all_issues()
+    connector.get_all_changes()
 
 
 if __name__ == "__main__":
